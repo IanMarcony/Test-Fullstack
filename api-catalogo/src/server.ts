cfa02fb88abf57baaae4cfa34d7aac992e54835c
IanMarcony <imarconyls@gmail.com>
2021-02-20 00:42:54 -0400

    Applied Cors

--- api-catalogo/src/server.ts
+++ api-catalogo/src/server.ts
@@ -10,8 +10,8 @@ import "./database";
 const app = express();
 const PORT = 3333;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 app.use(routes);
 app.use(
--- api-recomendacao/src/server.ts
+++ api-recomendacao/src/server.ts
@@ -9,8 +9,8 @@ import AppError from "./errors/AppError";
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 app.use(routes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof AppError) {
